461a2a1b3a75e858332fed28f57fb4468c02fc7e
Vincenzofdg <vfdgiacomo@gmail.com>
2022-02-07 07:57:10 -0300

    Images on src

--- src/data.js
+++ src/data.js
@@ -1,3 +1,16 @@
+// Importing My Pictures From Naruto's Movies
+import Ninja_Clash_In_The_Land_Of_Snow from './images/Ninja_Clash_In_The_Land_Of_Snow.png'
+import Legend_Of_The_Stone_Of_Gelel from './images/Legend_Of_The_Stone_Of_Gelel.jpg'
+import Guardians_Of_The_Crescent_Moon_Kingdom from './images/Guardians_Of_The_Crescent_Moon_Kingdom.png'
+import Naruto_Shippuden_The_Movie from './images/Naruto_Shippuden_The_Movie.jpg'
+import Bonds from './images/Bonds.jpg'
+import The_Will_Of_Fire from './images/The_Will_Of_Fire.png'
+import The_Lost_Tower from './images/The_Lost_Tower.jpg'
+import Blood_Prison from './images/Blood_Prison.png'
+import Road_To_Ninja from './images/Road_To_Ninja.jpg'
+import The_Last from './images/The_Last.jpg'
+import Boruto_Naruto_The_Movie from './images/Boruto_Naruto_The_Movie.jpg'
+
 const movies = [
   {
     title: 'Ninja Clash In The Land Of Snow',
@@ -5,7 +18,7 @@ const movies = [
     storyline: "",
     year: 2004,
     rating: 10.0,
-    imagePath: 'images/Ninja_Clash_In_The_Land_Of_Snow.jpg'
+    picture: Ninja_Clash_In_The_Land_Of_Snow
   },
   {
     title: 'Legend Of The Stone Of Gelel',
@@ -13,7 +26,7 @@ const movies = [
     storyline: "Team 7 is tasked with protecting the Gelel Stone, a powerful mineral that can grant its user regenerative abilities and slow down aging. The powers of the stone were once controlled by a clan, but wars over its power led them to extinction.",
     year: 2005,
     rating: 10.0,
-    imagePath: 'images/Legend_Of_The_Stone_Of_Gelel.jpg',
+    picture: Legend_Of_The_Stone_Of_Gelel,
   },
   {
     title: 'Guardians Of The Crescent Moon Kingdom',
@@ -21,7 +34,7 @@ const movies = [
     storyline: "Team 7 and Rock Lee are tasked with protecting Michiru, prince of the Land of the Moon, and his son Hikaru Tsuki. The team fought against Ishidate, a mercenary hired by the minister Shabadaba to kill the prince and his son.",
     rating: 10.0,
     year: 2006,
-    imagePath: 'images/Guardians_Of_The_Crescent_Moon_Kingdom.png',
+    picture: Guardians_Of_The_Crescent_Moon_Kingdom,
   },
   {
     title: 'Naruto Shippuden The Movie',
@@ -29,7 +42,7 @@ const movies = [
     storyline: "When the spirit of the demon Mōryō threatens to destroy the world, Team 7 is sent to protect Sion, a priestess who would be able to seal Mōryō's spirit away again and save the world.",
     rating: 10.0,
     year: 2007,
-    imagePath: 'images/Naruto_Shippuden_The_Movie.jpg',
+    picture: Naruto_Shippuden_The_Movie,
   },
   {
     title: 'Bonds',
@@ -37,7 +50,7 @@ const movies = [
     storyline: "After enemy ninjas from the Land of the Sky attack the Hidden Leaf Village as revenge for a previous attack during the 2nd Great Ninja World War, a boy named Amaru from another destroyed village comes to warn them. Naruto, Sakura, and Hinata protect the boy while trying to figure out why his village was destroyed.",
     rating: 10.0,
     year: 2008,
-    imagePath: 'images/Bonds.jpg',
+    picture: Bonds,
   },
   {
     title: 'The Will Of Fire',
@@ -45,7 +58,7 @@ const movies = [
     storyline: "The plot revolves around concerns of the potential outbreak of a Fourth Great Ninja World War when the four ninjas who guard the Hidden Villages of Rock, Cloud, Mist, and Sand go missing. The team fights against Hiruko, a ninja who abandoned the Leaf Village and wants to conquer the world after plunging it into war.",
     rating: 10.0,
     year: 2009,
-    imagePath: 'images/The_Will_Of_Fire.png',
+    picture: The_Will_Of_Fire,
   },
   {
     title: 'The Lost Tower',
@@ -53,7 +66,7 @@ const movies = [
     storyline: "After Team 7 is assigned on a mission to capture a missing ninja named Mukade, Naruto and Yamato are sent 20 years into the past. The film ends with the groups memories of the experienced being erased, so it ends up having very little influence on the main storyline of the franchise.",
     rating: 10.0,
     year: 2010,
-    imagePath: 'images/The_Lost_Tower.jpg',
+    picture: The_Lost_Tower,
   },
   {
     title: 'Blood Prison',
@@ -61,7 +74,7 @@ const movies = [
     storyline: "The movie opens with Naruto being falsely arrested for supposedly attacking the Fourth Raikage.",
     rating: 10.0,
     year: 2011,
-    imagePath: 'images/Blood_Prison.png',
+    picture: Blood_Prison,
   },
   {
     title: 'Road To Ninja',
@@ -69,7 +82,7 @@ const movies = [
     storyline: "Naruto and Sakura are transported into an alternate reality by Tobi using Limited Tsukuyomi. Fans get to see what the story would be like if Naruto's parents were still alive, Sasuke never left the Hidden Leaf village, and Sakura was an orphan instead of Naruto.",
     rating: 10.0,
     year: 2012,
-    imagePath: 'images/Road_To_Ninja.jpg',
+    picture: Road_To_Ninja,
   },
   {
     title: 'The Last',
@@ -77,7 +90,7 @@ const movies = [
     storyline: "It tales place two years after the Fourth Ninja World War, and opens with the moon heading to collide with the Earth, caused by Toneri Otsutsuki, a descendant of the Sage of Six Paths' twin brother Hamura Otsutsuki.",
     rating: 10.0,
     year: 2014,
-    imagePath: 'images/The_Last.jpg',
+    picture: The_Last,
   },
   {
     title: 'Boruto - Naruto The Movie',
@@ -85,7 +98,7 @@ const movies = [
     storyline: "Boruto forms a team with Sarada, the daughter of Sasuke and Sakura, and Mitsuki, the son of the villain Orochimaru. After Naruto sacrifices himself to stop two villains after the Nine-Tailed spirit inside his body, Sasuke and Boruto travel to another dimension to rescue Naruto.",
     rating: 10.0,
     year: 2015,
-    imagePath: 'images/Boruto_Naruto_The_Movie.jpg',
+    picture: Boruto_Naruto_The_Movie,
   },
 ];
 
